7803455ae05cc09f1920bc69f41d45843f2206e4
fleckenstein-m <16965213+fleckenstein-m@users.noreply.github.com>
2020-06-01 10:47:57 -0400

    Delete minbygroup.m

--- minbygroup.m
+++
@@ -1,5 +0,0 @@
-function [minval,midx] = minbygroup(val, validx)
-  % return min value of val, index in input list vector (typically 1:length(val))
-  [minval, midx] = min(val);    
-  midx = validx(midx);
-end
